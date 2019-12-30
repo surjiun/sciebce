d09c114ced1b71cc8135289c780ce44f81b71151
sjn <dgg7734@naver.com>
2019-12-30 15:07:32 +0900

    Make player move
    title is description

---
+++ _science/Assets/Scripts/MainGamePart/Player/PlayerHeadMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHeadMove : MonoBehaviour
+{
+    public static Vector3 mos;
+    //카메라 회전 속도
+    float xSpeed = 220.0f;
+    float ySpeed = 100.0f;
+    //카메라 초기 위치
+    private float x = 0.0f;
+    private float y = 0.0f;
+   
+    void Start()
+    {
+        {
+            Cursor.lockState = CursorLockMode.Locked; //커서 고정
+            Vector3 angles = transform.eulerAngles;
+
+            x = angles.y;
+            y = angles.x;
+        }//뚝배기 회전
+        
+    }
+    void FixedUpdate()
+    {
+        {
+            //카메라 회전속도 계산
+            x += Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
+            //카메라 위치 변화 계산
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+            Vector3 position = gameObject.transform.position + new Vector3(0.0f, 0, 0.0f);
+            transform.rotation = rotation;
+            gameObject.transform.rotation = Quaternion.Euler(0, x, 0);
+            transform.position = position;
+
+            //마우스 좌표
+            mos = Input.mousePosition;
+        }//뚝배기 회전
+    }
+}
--- _science/Assets/Scripts/MainGamePart/Player/PlayerMove.cs
+++ _science/Assets/Scripts/MainGamePart/Player/PlayerMove.cs
@@ -4,65 +4,38 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    /*public static Vector3 mos;
-    //추적할 대상
-    public Transform target;
-    //카메라 회전 속도
-    float xSpeed = 220.0f;
-    float ySpeed = 100.0f;
-    //카메라 초기 위치
-    private float x = 0.0f;
-    private float y = 0.0f;
-    //y값 제한 (위 아래 제한)
-    float yMinLimit = -20f;
-    float yMaxLimit = 80f;
-   
-    //앵글의 최소,최대 제한
-    float ClampAngle(float angle, float min, float max)
-    {
-        if (angle < -360)
-            angle += 360;
-        if (angle > 360)
-            angle -= 360;
-        return Mathf.Clamp(angle, min, max);
-    }
-    void Start()
-    {
-        {
-            Cursor.lockState = CursorLockMode.Locked; //커서 고정
-            Vector3 angles = transform.eulerAngles;
+    float runGage = 100.0f;
+    int Speed = 5;
 
-            x = angles.y;
-            y = angles.x;
-        }//뚝배기 회전
-        
-    }
-    void FixedUpdate()
-    {
-        {
-            //카메라 회전속도 계산
-            x += Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
-            y -= Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
-            //앵글값 정하기(y값 제한)
-            y = ClampAngle(y, yMinLimit, yMaxLimit);
-            //카메라 위치 변화 계산
-            Quaternion rotation = Quaternion.Euler(y, x, 0);
-            Vector3 position = rotation * new Vector2(0, 0.9f) + target.position + new Vector3(0.0f, 0, 0.0f);
-            transform.rotation = rotation;
-            target.rotation = Quaternion.Euler(0, x, 0);
-            transform.position = position;
-
-
-            //마우스 좌표
-            mos = Input.mousePosition;
-        }//뚝배기 회전
-    }*/
     void Update()
     {
-            Vector3 pos = Input.mousePosition;
-            pos.z = 10;
-            Vector3 target = Camera.main.ScreenToWorldPoint(pos);
-            print("target" + target);
-            transform.LookAt(target);
-    } 
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift) && runGage > 0)
+        {
+            Speed = 8;
+            runGage -= 2 * Time.deltaTime;
+        }         
+        else
+        {
+            Speed = 5;
+            runGage +=  2 * Time.deltaTime;
+        }
+            
+    }
 }
